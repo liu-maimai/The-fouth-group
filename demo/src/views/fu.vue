b34aa55bb2e0a1e36a4eb1c8d7c2b16723989a5d
Mr.fu <770159889@qq.com>
2021-01-14 07:31:50 +0800

    7.31早上提交

---
+++ demo/src/views/fu.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>越来越暖和了</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
